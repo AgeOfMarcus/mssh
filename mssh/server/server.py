52369706aaeaa26db60a54e7a856cf147a1e17b1
Marcus Weinberger <marcus@localhost.localdomain>
2018-11-21 18:50:15 +0000

    new commit

--- mssh/server/server.py
+++ mssh/server/server.py
@@ -1,5 +1,7 @@
 from hackerman.transport import tcp
 from hackerman.crypto import xor, rsa, blowfish
+from subprocess import Popen, PIPE
+import os, signal
 
 from mssh import messages as msg
 from mssg import errors
@@ -15,7 +17,22 @@ class Server(object):
 		self.enc = enc_func 
 		self.dec = dec_func
 
+		quit = False
+
 		while True:
 			cmd = self.dec(self.conn.recv()).decode()
 
-			pipe = utils.interactive_sh #CONT
+			proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+			for ln in iter(proc.stdout.readline, b''):
+				if self.dec(self.conn.recv()) == msg.ctrl_c:
+					os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+					self.conn.send(self.enc(ln))
+					self.conn.send(self.enc(msg.ctrl_c))
+					nope = True
+					break
+				else:
+					self.conn.send(self.enc(ln))
+
+
+			if not nope: self.conn.send(self.enc(msg.end_t))
+			if nope: nope = False
